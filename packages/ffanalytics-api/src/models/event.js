aef9c7a4b7c1596df8efa3c00cf1b76f1c5e4170
delwiv <delwiv@protonmail.com>
2024-03-14 23:26:24 +0100

    feat: event=>strict:false

--- packages/ffanalytics-api/src/models/event.js
+++ packages/ffanalytics-api/src/models/event.js
@@ -1,19 +1,25 @@
 import mongoose from 'mongoose'
 
-const Event = mongoose.model('Event', {
-  name: String,
-  type: String,
-  userAgent: String,
-  date: {
-    type: Date,
-    default: Date.now,
+const Event = mongoose.model(
+  'Event',
+  {
+    name: String,
+    type: String,
+    userAgent: String,
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    payload: {},
+    ip: String,
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Client',
+    },
   },
-  payload: {},
-  ip: String,
-  client: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Client',
-  },
-})
+  {
+    strict: false,
+  }
+)
 
 export default Event
